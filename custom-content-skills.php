177aab293a1a60a23a89eb1fbefb6cf245de94e6
Kurt Simmons <ksimmons@alphaworks.tech>
2022-10-12 16:54:06 -0700

    feat(pages and content): finished header and footer for desktop

---
+++ custom-content-skills.php
@@ -0,0 +1,25 @@
+<div class="row">
+      <div class="col col-sm-12 ">
+        <p class="chart_text">Front-end</p>
+        <div class="skillbox">
+          <div class="skill_level1"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p class="chart_text">Back-end</p>
+        <div class="skillbox">
+          <div class="skill_level2"></div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p class="chart_text">Creativity</p>
+        <div class="skillbox">
+          <div class="skill_level3"></div>
+        </div>
+      </div>
+    </div>
+  </div>
